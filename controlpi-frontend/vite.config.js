d1926703f94943fc21fbf54edde64b2d81574a91
Nathan-Joy KAYEMBE NDALAMBA <nathanjoyn@gmail.com>
2025-11-29 20:25:27 +0200

    Nettoyage final: suppression des fichiers parasites

--- controlpi-frontend/vite.config.js
+++ controlpi-frontend/vite.config.js
@@ -3,17 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
   server: {
-    port: 5173,
-    open: true,
-  },
+    port: 5173,   // fixe le port
+    open: true    // ouvre automatiquement le navigateur
+   },
   build: {
     outDir: "dist",
     sourcemap: true,
